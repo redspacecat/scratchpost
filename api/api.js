ee0ee31ca558631cedc8ccb28f469e4506c681ea
redspacecat <162653322+redspacecat@users.noreply.github.com>
2025-11-22 09:02:36 -0800

    add timestamps

--- api/api.js
+++ api/api.js
@@ -15,13 +15,31 @@ api.topic = async function(request, reply) {
     const postsOutput = []
     for (const post of posts) {
         let postData = {}
+
+        let timeStr = post.querySelector(".box-head").children[1].innerText
+        let timestamp
+        if (timeStr.includes("Today") || timeStr.includes("Yesterday")) {
+            if (timeStr.includes("Today")) {
+                const today = new Date().toLocaleDateString()
+                timestamp = new Date(`${today} ${timeStr.split(" ")[1]} EST`).getTime()
+            } else {
+                const yesterday = new Date(new Date().setDate(new Date().getDate() - 1)).toLocaleDateString()
+                timestamp = new Date(`${yesterday} ${timeStr.split(" ")[1]} EST`).getTime()
+            }
+        } else {
+            timestamp = new Date(timeStr).getTime()
+        }
+
         postData.author = {}
         postData.author.username = post.querySelector(".black.username").innerText
         postData.author.id = parseInt(post.querySelector(".postavatar").querySelector("img").getAttribute("src").split("/")[5].split("_")[0])
+        postData.author.postCount = Array.from(post.querySelector(".postleft").querySelector("dl").childNodes).at(-1).textContent.trim().split(" ")[0]
+        postData.author.rank = Array.from(post.querySelector(".postleft").querySelector("dl").childNodes).at(-3).textContent.trim().split(" ")[0]
         postData.id = parseInt(post.id.slice(1))
         postData.content = post.querySelector(".post_body_html").innerHTML
         postData.number = parseInt(post.querySelector(".conr").innerText.slice(1))
-        postData.time = post.querySelector(".box-head").children[1].innerText
+        postData.time = timeStr
+        postData.timestamp = timestamp
         postsOutput.push(postData)
     }
     return JSON.stringify(postsOutput, null, 4)
--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,64 @@
 function htmlToNode(htmlString) {
-  var div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
+    var div = document.createElement("div");
+    div.innerHTML = htmlString.trim();
 
-  return div.firstChild;
+    return div.firstChild;
+}
+
+function handlePostCounts(data) {
+    const counts = {};
+    for (const item of data) {
+        const pieces = item.split(",");
+        counts[pieces[0]] = parseInt(pieces[1]);
+    }
+    return counts;
 }
 
 (async function () {
     const topic = location.pathname.split("/").at(-1);
     const page = new URLSearchParams(location.search).get("page") ?? 1;
-    let [postCounts, posts] = await Promise.all([
-        await fetch("https://raw.githubusercontent.com/redspacecat/scratch-forums-data/main/post_counts.txt"),
-        await fetch(`/api/topic/${topic}?page=${page}`)
-    ])
-    postCounts = await postCounts.text()
-    posts = await posts.json()
-    const container = document.querySelector("#posts_container")
+    let [postCounts, posts] = await Promise.all([await fetch("https://raw.githubusercontent.com/redspacecat/scratch-forums-data/main/post_counts.txt"), await fetch(`/api/topic/${topic}?page=${page}`)]);
+    postCounts = await postCounts.text();
+    postCounts = handlePostCounts(postCounts.split("\n"));
+    console.log(postCounts);
+    posts = await posts.json();
+    const container = document.querySelector("#posts_container");
 
     for (const post of posts) {
         const basePost = `<div class="post">
-    <div class="date"></div>
+    <div class="post-top">
+        <span class="date"></span>
+        <span class="post-number"></span>
+    </div>
     <div class="container">
         <div class="left">
             <div class="username"></div>
             <img id="user-img">
+            <br>
+            <span class="post-count"></span>
         </div>
         <div class="right">
             <div class="content"></div>
         </div>
     </div>
-</div>`
-console.log(basePost.replaceAll(/\n[ ]+/g, ""))
-        const newEl = htmlToNode(basePost.replaceAll(/\n[ ]+/g, ""))
-        console.log(newEl)
-        newEl.querySelector(".date").innerText = post.time
-        newEl.querySelector(".username").innerText = post.author.username
-        newEl.querySelector("img").src = `https://cdn2.scratch.mit.edu/get_image/user/${post.author.id}_90x90.png`
-        newEl.querySelector(".content").innerHTML = post.content
-        container.appendChild(newEl)
+</div>`;
+        console.log(basePost.replaceAll(/\n[ ]+/g, ""));
+        const newEl = htmlToNode(basePost.replaceAll(/\n[ ]+/g, ""));
+        console.log(newEl);
+        newEl.querySelector(".date").innerText = post.time;
+        newEl.querySelector(".post-number").innerText = "#" + post.number;
+        newEl.querySelector(".username").innerText = post.author.username;
+        newEl.querySelector("img").src = `https://cdn2.scratch.mit.edu/get_image/user/${post.author.id || "default"}_90x90.png`;
+        newEl.querySelector(".post-count").innerText = "~" + parseInt(postCounts[post.author.username] || 1) + " posts";
+        newEl.querySelector(".content").innerHTML = post.content;
+        container.appendChild(newEl);
     }
+
+    scratchblocks.renderMatching(".blocks", {
+        scale: 0.675,
+        languages: ["en", "de", "es", "fr", "zh_cn", "pl", "ja", "nl", "pt", "it", "he", "ko", "nb", "tr", "el", "ru", "ca", "id"],
+        style: "scratch3",
+    });
+
+    document.querySelector("h2").remove()
 })();
